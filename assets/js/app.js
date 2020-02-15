223359a418387f036f2470db56df7f916e556afb
romerojas <rdjrome@gmail.com>
2020-02-15 12:03:09 +0800

    added some files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,34 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+//Disable Nav and Scroll
+var noBodyOver = document.getElementsByTagName("body")[0];
+var naav = document.getElementById("header");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "images/fulls/resume.jpg";
+  naav.style.display = "none";
+  document.getElementsByTagName('html')[0].style.overflow = "hidden";
+}
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var noBodyOver = document.getElementsByTagName("body")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  noBodyOver.style.overflow = "hidden";
-
-}
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function() { 
   modal.style.display = "none";
-  noBodyOver.style.overflow = "auto";
+  naav.style.display = "block";
+  document.getElementsByTagName('html')[0].style.overflow = "scroll";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    noBodyOver.style.overflow = "auto";
+    naav.style.display = "block";
+    document.getElementsByTagName('html')[0].style.overflow = "scroll";
   }
 }
